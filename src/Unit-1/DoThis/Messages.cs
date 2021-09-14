d176e7d74493cef833906f6023a9c1efbf2f5f8f
ariel <ariel.viera@gmail.com>
2021-09-14 11:14:02 -0400

    Added message types

---
+++ src/Unit-1/DoThis/Messages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinTail
+{
+    #region neutral/system messages
+
+    /// <summary>
+    /// Marker class to continue processing
+    /// </summary>
+    public class ContinueProcessing
+    {
+
+    }
+    #endregion
+
+    #region success messages
+
+    public class InputSuccess
+    {
+        public InputSuccess(string reason)
+        {
+
+        }
+    }
+    #endregion
+
+    #region failure messages
+    #endregion
+}
